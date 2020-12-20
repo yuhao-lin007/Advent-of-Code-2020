f9252459975061f07b1f11f88ecb61be483865f9
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-12-20 01:10:48 +0000

    Day 1

---
+++ Day 1/day_1.py
@@ -0,0 +1,57 @@
+from math import prod
+
+def get_valid_entries(entries, nums_used, total_required):
+    length = len(entries)
+    
+    if nums_used < 1 or nums_used > length:
+        return
+
+    last_indice_index = nums_used - 1
+    indices = [i for i in range(nums_used)]
+
+    checks = 0
+    while True:
+        #print(f"Indices: {indices} ({checks})")
+
+        checks += 1
+        # Check if numbers based on index values totals to required value
+        numbers = [entries[index] for index in indices]
+        if sum(numbers) == total_required:
+            #print(f"Checks: {checks}")
+            return numbers
+
+        indices[last_indice_index] += 1
+
+        # Overflow to next index(es) if required
+        for i in range(nums_used-1, 0, -1):
+            min_index = max(indices[:i]) + 1
+            max_index = length - nums_used + i
+            if indices[i] > max_index:
+                indices[i-1] += 1
+                indices[i] = min_index
+
+        # Check if it has reached the end
+        if indices[0] > length - nums_used:
+            #print(f"Checks: {checks}")
+            return
+
+total_required = 2020
+
+with open("input.txt", "r") as file:
+    entries = [int(entry) for entry in file]
+
+# Task 1
+print("Task 1")
+valid_entries = get_valid_entries(entries, 2, total_required)
+print("Valid Entries:", valid_entries)
+if valid_entries:
+    print("Valid Entries Product:", prod(valid_entries))
+
+print()
+
+# Task 2
+print("Task 2")
+valid_entries = get_valid_entries(entries, 3, total_required)
+print("Valid Entries:", valid_entries)
+if valid_entries:
+    print("Valid Entries Product:", prod(valid_entries))
